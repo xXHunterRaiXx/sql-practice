c4b3f19863a27228213f52b2a6c452767d63b10e
Arianna Ninh <ariannaninh@gmail.com>
2025-06-09 15:58:33 -0700

    Update sql-noir-case-001-vanishing-briefcase.sql

--- sql-noir-case-001-vanishing-briefcase.sql
+++ sql-noir-case-001-vanishing-briefcase.sql
@@ -1,2 +1,4 @@
--- Record your SQL detective process here!  
--- Write down the SQL queries you ran, any notes or insights as SQL comments, and your final conclusion: who did it? 
+-- Record your SQL detective process here!  Write down: 
+  -- The SQL queries you ran
+  -- Any notes or insights as SQL comments
+  -- Your final conclusion: who did it?
