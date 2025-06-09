6101cbb0795510cb5a77ad33768e8887c39b1748
Arianna Ninh <ariannaninh@gmail.com>
2025-06-09 16:00:11 -0700

    Update sql-noir-case-004-vanishing-diamond.sql

--- sql-noir-case-004-vanishing-diamond.sql
+++ sql-noir-case-004-vanishing-diamond.sql
@@ -1,2 +1,4 @@
--- Record your SQL detective process here!  
--- Write down the SQL queries you ran, any notes or insights as SQL comments, and your final conclusion: who did it? 
+-- Record your SQL detective process here!  Write down: 
+  -- 1. The SQL queries you ran
+  -- 2. Any notes or insights as SQL comments
+  -- 3. Your final conclusion: who did it?
