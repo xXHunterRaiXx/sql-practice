4a9ee975f95100c0366a7de7ca543a00f9e86f33
Arianna Ninh <ariannaninh@gmail.com>
2025-06-09 08:35:10 -0700

    Create wix-cookie-shop.sql

---
+++ wix-cookie-shop.sql
@@ -0,0 +1,53 @@
+-- For each comment below, write the correct SQL query directly underneath it.
+-- After writing each SQL command, run your code to check for errors and make sure it works before moving on to the next one.
+
+---------------------------------------------------
+
+    -- CREATE the products table for the Wix Cookie Shop store. 
+    -- Use your data model spreadsheet to help you choose the column names and data types.
+
+
+    -- INSERT at least 3 rows of sample data for the products table.
+
+
+    -- SELECT all columns from the products table. 
+
+      
+    -- CREATE the customers table for the Wix Cookie Shop store. 
+    -- Use your data model spreadsheet to help you choose the column names and data types.
+
+
+    -- INSERT at least 3 rows of sample data for the customers table.
+
+
+    -- SELECT all columns from the customers table. 
+  
+    
+    -- CREATE the orders table for the Wix Cookie Shop store. 
+    -- Use your data model spreadsheet to help you choose the column names and data types.
+    
+    
+    -- INSERT at least 3 rows of sample data for the orders table.
+
+
+    -- SELECT all columns from the orders table. 
+
+
+    -- SELECT the names and emails from the customers table. 
+  
+
+    -- SELECT the names of all the products that cost $1.00. 
+   
+    
+    -- UPDATE the name of a product in the products table. 
+    -- You can choose which product you want to rename. 
+  
+
+    -- SELECT all columns from the products table. You should see your updated product in the table. 
+    
+    
+    -- DELETE one of orders from the orders table.
+    -- You can choose which order you want to delete. 
+
+
+    -- SELECT all columns from the orders table. You should see the order you deleted is now gone from the table. 
