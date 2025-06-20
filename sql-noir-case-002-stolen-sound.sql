bed29b5c6048f3d51d0f06fd6fed037812a0350c
Hunter-Rai <166153827+xXHunterRaiXx@users.noreply.github.com>
2025-06-20 14:24:39 -0500

    Update sql-noir-case-002-stolen-sound.sql
    update case 2 of sql detective work

--- sql-noir-case-002-stolen-sound.sql
+++ sql-noir-case-002-stolen-sound.sql
@@ -1,4 +1,11 @@
--- Record your SQL detective process here!  Write down: 
-  -- 1. The SQL queries you ran
-  -- 2. Any notes or insights as SQL comments
-  -- 3. Your final conclusion: who did it?
+SELECT * FROM crime_scene WHERE date = '19830715'
+ -- 65	19830715	theft	West Hollywood Records	A prized vinyl record was stolen from the store during a busy evening.
+  SELECT * FROM witnesses WHERE crime_scene_id = 65 
+ -- 28	65	I saw a man wearing a red bandana rushing out of the store.
+--75	65	The main thing I remember is that he had a distinctive gold watch on his wrist.
+  SELECT * FROM suspects WHERE bandana_color = 'red' AND  accessory = 'gold watch'
+-- 35	Tony Ramirez	red	gold watch
+--44	Mickey Rivera	red	gold watch
+--97	Rico Delgado	red	gold watch
+SELECT * FROM interviews 
+--Rico Delgado confessed to stealing in the interview 
