73cd42f1a69c5a3c92538e9d281ea6b1050cd6d0
Hunter-Rai <166153827+xXHunterRaiXx@users.noreply.github.com>
2025-06-20 14:19:59 -0500

    Update sql-noir-case-001-vanishing-briefcase.sql
    update sql for case 1

--- sql-noir-case-001-vanishing-briefcase.sql
+++ sql-noir-case-001-vanishing-briefcase.sql
@@ -2,3 +2,12 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+SELECT * FROM suspects WHERE attire = 'trench coat';
+--3	Frankie Lombardi	trench coat	left cheek
+--183	Vincent Malone	trench coat	left cheek
+SELECT * FROM crime_scene WHERE type = 'theft' AND location = 'Blue Note Lounge';
+id	date	type	location	description
+--76	19851120	theft	Blue Note Lounge	A briefcase containing sensitive documents vanished.
+--A witness reported a man in a trench coat with a scar on his left cheek fleeing the scene.
+  SELECT * FROM interviews;
+-- Vincent Malone confessed to stealing the item in interview he said he wasnt going to buut he did
